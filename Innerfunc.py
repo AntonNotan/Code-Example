bf9dcd1fed7a51c82563252f4813897ae02e2d7a
AntonNotan <55237939+AntonNotan@users.noreply.github.com>
2019-09-13 15:26:50 +0300

    Third Function

---
+++ Innerfunc.py
@@ -0,0 +1,32 @@
+def Merge(A,p,q,r):
+    B=[]
+
+    if A[r-1]<=A[p-1]:
+        B = A[:]
+        if (p + r) % 2 != 0:
+            for i in range(q, r):
+                A[p + i - q - 1] = B[i]
+            for i in range(p - 1, q):
+                A[i + (q - (p - 1))] = B[i]
+        else:
+            for i in range(q, r):
+                A[p + i - q - 1] = B[i]
+            for i in range(p - 1, q):
+                A[i + (q - (p))] = B[i]
+
+    elif A[q]<A[q-1]:
+        j=p-1
+        for i in range(q, r):
+            if j < i:
+                while (A[j] <= A[i]) and (j < i):
+                    j += 1
+                if j == i:
+                    break
+                Q = A[j]
+                A[j] = A[i]
+                if (i - j) > 1:
+                    for k in range(1, i - j):
+                        A[i + 1 - k] = A[i - k]
+                j += 1
+                A[j] = Q
+        return A
---
+++ Sorting.py
@@ -0,0 +1,11 @@
+from Innerfunc import Merge
+
+def Sort(A,p,r):
+    if p<r:
+        q=round((p+r)/2-0.01)
+        print(p,q)
+        Sort(A,p,q)
+        print(q+1,r)
+        Sort(A,q+1,r)
+        Merge(A,p,q,r)
+        return A
